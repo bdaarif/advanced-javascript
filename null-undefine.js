038d0b4eea917b96e3937591ff7981314b9e83a8
ariful7885 <arifulislam2376@gmail.com>
2020-03-05 06:25:48 +0600

    null vs undefined for interview

---
+++ null-undefine.js
@@ -0,0 +1,22 @@
+// How find undefined value
+// first_step
+let cow;
+console.log(cow);
+
+// second_step
+function add (num1,num2){
+    console.log(num1,num2);
+    
+}
+const sum = add(12);
+console.log(sum);
+
+// Third_step
+const mithila = {
+    name:"aishi", 
+    phone:01772
+};
+console.log(mithila.gender);
+
+const my = undefined;
+console.log(my);
